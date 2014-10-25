dfe5e10696ea3f980a11071cae193b046b3b50ad
lukfor <lukas.forer@i-med.ac.at>
2014-10-25 13:44:05 +0200

    Updated version.

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
 	private static final Log log = LogFactory.getLog(Main.class);
 
-	public static final String VERSION = "1.9.3";
+	public static final String VERSION = "1.9.5";
 
 	public static void main(String[] args) throws IOException {
 
