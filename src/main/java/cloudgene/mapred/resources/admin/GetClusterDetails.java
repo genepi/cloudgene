28db2e483e77cd1bcc659d0108ad3ebada8e5db2
lukfor <lukas.forer@i-med.ac.at>
2015-03-05 10:49:41 +0100

    Added version field.

--- src/main/java/cloudgene/mapred/resources/admin/GetClusterDetails.java
+++ src/main/java/cloudgene/mapred/resources/admin/GetClusterDetails.java
@@ -1,5 +1,11 @@
 package cloudgene.mapred.resources.admin;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 import net.sf.json.JSONObject;
 
 import org.restlet.data.MediaType;
@@ -9,6 +15,7 @@ import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import cloudgene.mapred.Main;
 import cloudgene.mapred.core.User;
 import cloudgene.mapred.core.UserSessions;
 import cloudgene.mapred.jobs.WorkflowEngine;
@@ -37,6 +44,23 @@ public class GetClusterDetails extends ServerResource {
 		}
 
 		JSONObject object = new JSONObject();
+		object.put("version", Main.VERSION);
+
+		URLClassLoader cl = (URLClassLoader) Main.class.getClassLoader();
+		try {
+			URL url = cl.findResource("META-INF/MANIFEST.MF");
+			Manifest manifest = new Manifest(url.openStream());
+			Attributes attr = manifest.getMainAttributes();
+			String buildVesion = attr.getValue("Version");
+			String buildTime = attr.getValue("Build-Time");
+			String builtBy = attr.getValue("Built-By");
+			object.put("built_by", builtBy);
+			object.put("built_time", buildTime);
+
+		} catch (IOException E) {
+			// handle
+		}
+
 		object.put("maintenance", settings.isMaintenance());
 		object.put("blocked", !WorkflowEngine.getInstance().isRunning());
 		object.put("threads", settings.getThreadsQueue());
