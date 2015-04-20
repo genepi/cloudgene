079e13f78864bde0f42e5adf6a3e3aefb8a93fce
lukfor <lukas.forer@i-med.ac.at>
2015-04-20 10:32:17 +0200

    Fixed problem with usergroups.

--- src/main/html/webapp/assets/js/AdminUsersPage.js
+++ src/main/html/webapp/assets/js/AdminUsersPage.js
@@ -5,38 +5,58 @@ User = can.Model({
 	findAll : 'GET /users'
 }, {});
 
-
-
 // controller
-AdminUsersPage = can.Control({
+AdminUsersPage = can
+		.Control({
 
-	"init" : function(element, options) {
-		var that = this;
-		User.findAll({
-			state : "failed"
-		}, function(users) {
-			that.element.html(can.view('/views/admin/users.ejs', users));
-			$("#content").fadeIn();
-		}, function(message) {
-			new ErrorPage(that.element, {
-				status : message.statusText,
-				message : message.responseText
-			});
-		});
+			"init" : function(element, options) {
+				var that = this;
+				User.findAll({
+					state : "failed"
+				}, function(users) {
+					that.element
+							.html(can.view('/views/admin/users.ejs', users));
+					$("#content").fadeIn();
+				}, function(message) {
+					new ErrorPage(that.element, {
+						status : message.statusText,
+						message : message.responseText
+					});
+				});
 
-	},
+			},
 
-	'.icon-trash click' : function(el, ev) {
+			'.icon-trash click' : function(el, ev) {
 
-		user = el.parent().parent().data('user');
-		bootbox.animate(false);
-		bootbox.confirm("Are you sure you want to delete <b>"
-				+ user.attr('username') + "</b>?", function(result) {
-			if (result) {
-				user.destroy();
+				user = el.parent().parent().data('user');
+				bootbox.animate(false);
+				bootbox.confirm("Are you sure you want to delete <b>"
+						+ user.attr('username') + "</b>?", function(result) {
+					if (result) {
+						user.destroy();
+					}
+				});
+
+			},
+
+			'.icon-pencil click' : function(el, ev) {
+
+				user = el.parent().parent().data('user');
+				var oldText = user.attr('role');
+				bootbox
+						.confirm(
+								'<h4> Change role of '
+										+ user.attr('username')
+										+ '</h4><form><input type="text"class="field span2" id="message" name="message" value="'
+										+ oldText + '">'
+										+ '</imput></form>', function(result) {
+									if (result) {
+										var text = $('#message').val();
+										user.attr('role', text);
+										user.save();
+									}
+								});
+
 			}
+
 		});
-
-	}
-
-});
