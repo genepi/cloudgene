ffbd6857533df4f88adb3a9cb81b8f50f85befb4
lukfor <lukas.forer@i-med.ac.at>
2015-05-04 16:38:42 +0200

    Added loggedIn variable.

--- src/main/html/webapp/assets/js/HomePage.js
+++ src/main/html/webapp/assets/js/HomePage.js
@@ -13,7 +13,7 @@ HomePage = can.Control({
 		that = this;
 		Counter.findOne({}, function(counter) {
 			that.element.hide();
-			that.element.html(can.view('/static/home.ejs',{counter: counter}));
+			that.element.html(can.view('/static/home.ejs',{counter: counter, loggedIn: !options.login}));
 			that.element.fadeIn();
 			if (options.login) {
 				new UserLoginForm("#login-form");
