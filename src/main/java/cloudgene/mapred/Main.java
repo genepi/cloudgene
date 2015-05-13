06030da0206490f2d43aab2724277d8c13031c2c
lukfor <lukas.forer@i-med.ac.at>
2015-05-13 11:13:11 +0200

    Updated version.

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -41,7 +41,7 @@ public class Main implements Daemon {
 
 	// private static final Log log = LogFactory.getLog(Main.class);
 
-	public static final String VERSION = "1.9.7";
+	public static final String VERSION = "1.9.9";
 
 	private Database database;
 
