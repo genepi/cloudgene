dbdb222d9e989012e0b4d26929ac72aaf28edf0e
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2015-10-27 15:44:46 +0100

    update to 1.1.10; adding secure ciphers

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -41,7 +41,7 @@ public class Main implements Daemon {
 
 	// private static final Log log = LogFactory.getLog(Main.class);
 
-	public static final String VERSION = "1.9.9";
+	public static final String VERSION = "1.10.0";
 
 	private Database database;
 
