54fc2e50010c85f305e78d7857ea460f2be8569b
lukfor <lukas.forer@i-med.ac.at>
2015-12-29 11:09:23 +0100

    Removed per user limit in public mode.

--- src/main/java/cloudgene/mapred/resources/jobs/NewSubmitJob.java
+++ src/main/java/cloudgene/mapred/resources/jobs/NewSubmitJob.java
@@ -38,285 +38,294 @@ import cloudgene.mapred.wdl.WdlReader;
 public class NewSubmitJob extends BaseResource {
 
 	boolean publicMode = false;
-	
+
 	@Post
 	public Representation post(Representation entity) {
-try{
-		User user = getUser(getRequest());
-		String tool = getAttribute("tool");
-
-		String filename = getSettings().getApp(user, tool);
-		WdlApp app = null;
 		try {
-			app = WdlReader.loadAppFromFile(filename);
-		} catch (Exception e1) {
+			User user = getUser(getRequest());
+			String tool = getAttribute("tool");
 
-			setStatus(Status.CLIENT_ERROR_NOT_FOUND);
-			return new StringRepresentation("Tool '" + tool + "' not found or the request requires user authentication.");
+			String filename = getSettings().getApp(user, tool);
+			WdlApp app = null;
+			try {
+				app = WdlReader.loadAppFromFile(filename);
+			} catch (Exception e1) {
 
-		}
-		
-		if (user == null){
-			
-			publicMode = true;
-			
-			UserDao dao = new UserDao(getDatabase());
-			user = dao.findByUsername("public");
+				setStatus(Status.CLIENT_ERROR_NOT_FOUND);
+				return new StringRepresentation(
+						"Tool '"
+								+ tool
+								+ "' not found or the request requires user authentication.");
+
+			}
+
 			if (user == null) {
-				user = new User();
-				user.setUsername("public");
-				String password = HashUtil.getMD5("public-password");
-				user.setPassword(password);
-				user.setRole("public");
-				dao.insert(user);
-			}
-		}
-		
 
-		WorkflowEngine engine = getWorkflowEngine();
+				publicMode = true;
 
-		Settings settings = getSettings();
-
-		if (engine.getActiveCount() >= settings.getMaxRunningJobs()) {
-
-			JSONObject answer = new JSONObject();
-			try {
-				answer.put("success", false);
-				answer.put("message",
-						"More than " + settings.getMaxRunningJobs()
-								+ "  jobs are currently in the queue.");
-			} catch (Exception e) {
-				e.printStackTrace();
+				UserDao dao = new UserDao(getDatabase());
+				user = dao.findByUsername("public");
+				if (user == null) {
+					user = new User();
+					user.setUsername("public");
+					String password = HashUtil.getMD5("public-password");
+					user.setPassword(password);
+					user.setRole("public");
+					dao.insert(user);
+				}
 			}
 
-			return new StringRepresentation(answer.toString());
+			WorkflowEngine engine = getWorkflowEngine();
 
-		}
+			Settings settings = getSettings();
 
-		if (engine.getJobsByUser(user).size() >= settings
-				.getMaxRunningJobsPerUser()) {
+			if (engine.getActiveCount() >= settings.getMaxRunningJobs()) {
 
-			JSONObject answer = new JSONObject();
-			try {
-				answer.put("success", false);
-				answer.put(
-						"message",
-						"Only "
-								+ settings.getMaxRunningJobsPerUser()
-								+ " jobs per user can be executed simultaneously.");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return new StringRepresentation(answer.toString());
-
-		}
-
-		// tring tool = "minimac/new-simple";// props.get("tool");
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
-		String id = "job-" + sdf.format(new Date());// props.get("job-name");
-		
-		if (publicMode){
-			id = HashUtil.getMD5(id+"-lukas");
-		}
-		
-		String hdfsWorkspace = HdfsUtil.path(getSettings().getHdfsWorkspace(),
-				user.getUsername());
-		String localWorkspace = FileUtil.path(
-				getSettings().getLocalWorkspace(), user.getUsername());
-
-		Map<String, String> props = new HashMap<String, String>();
-
-		try {
-			FileItemIterator iterator = parseRequest(entity);
-
-			// uploaded files
-			while (iterator.hasNext()) {
-
-				FileItemStream item = iterator.next();
-
-				String name = item.getName();
-
-				if (name != null) {
-
-					// file parameter
-					// write local file
-					String tmpFile = getSettings().getTempFilename(
-							item.getName());
-					File file = new File(tmpFile);
-					try {
-						FileUtils
-								.copyInputStreamToFile(item.openStream(), file);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-
-					// import into hdfs
-					if (file.exists()) {
-						String entryName = item.getName();
-
-						// remove upload indentification!
-						String fieldName = item.getFieldName()
-								.replace("-upload", "").replace("input-", "");
-
-						boolean hdfs = false;
-						boolean folder = false;
-
-						for (WdlParameter input : app.getMapred().getInputs()) {
-							if (input.getId().equals(fieldName)) {
-								hdfs = (input.getType().equals(
-										WdlParameter.HDFS_FOLDER) || input
-										.getType().equals(
-												WdlParameter.HDFS_FILE));
-								folder = (input.getType()
-										.equals(WdlParameter.HDFS_FOLDER))
-										|| (input.getType()
-												.equals(WdlParameter.LOCAL_FOLDER));
-							}
-						}
-
-						if (hdfs) {
-
-							String targetPath = HdfsUtil.path(hdfsWorkspace,
-									"input", id, fieldName);
-
-							String target = HdfsUtil
-									.path(targetPath, entryName);
-
-							HdfsUtil.put(tmpFile, target);
-
-							// deletes temporary file
-							FileUtil.deleteFile(tmpFile);
-
-							if (folder) {
-								// folder
-								props.put(fieldName,
-										HdfsUtil.path("input", id, fieldName));
-							} else {
-								// file
-								props.put(fieldName, HdfsUtil.path("input", id,
-										fieldName, entryName));
-							}
-
-						} else {
-
-							String targetPath = FileUtil.path(localWorkspace,
-									"input", id, fieldName);
-
-							FileUtil.createDirectory(FileUtil.path(
-									localWorkspace, "input"));
-
-							FileUtil.createDirectory(FileUtil.path(
-									localWorkspace, "input", id));
-
-							FileUtil.createDirectory(FileUtil.path(
-									localWorkspace, "input", id, fieldName));
-
-							String target = FileUtil
-									.path(targetPath, entryName);
-
-							FileUtil.copy(tmpFile, target);
-
-							// deletes temporary file
-							FileUtil.deleteFile(tmpFile);
-
-							if (folder) {
-								// folder
-								props.put(
-										fieldName,
-										new File(FileUtil.path(localWorkspace,
-												"input", id, fieldName))
-												.getAbsolutePath());
-							} else {
-								// file
-								props.put(
-										fieldName,
-										new File(FileUtil.path(localWorkspace,
-												"input", id, fieldName,
-												entryName)).getAbsolutePath());
-							}
-
-						}
-
-					}
-
-				} else {
-
-					if (item.getFieldName().startsWith("input-")) {
-						String key = item.getFieldName().replace("input-", "");
-
-						String value = Streams.asString(item.openStream());
-						if (!props.containsKey(key)) {
-							// don't override uploaded files
-							props.put(key, value);
-						}
-
-					}
-
+				JSONObject answer = new JSONObject();
+				try {
+					answer.put("success", false);
+					answer.put("message",
+							"More than " + settings.getMaxRunningJobs()
+									+ "  jobs are currently in the queue.");
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 
+				return new StringRepresentation(answer.toString());
+
 			}
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			if (!publicMode
+					&& engine.getJobsByUser(user).size() >= settings
+							.getMaxRunningJobsPerUser()) {
 
-		if (app.getMapred() != null) {
+				JSONObject answer = new JSONObject();
+				try {
+					answer.put("success", false);
+					answer.put(
+							"message",
+							"Only "
+									+ settings.getMaxRunningJobsPerUser()
+									+ " jobs per user can be executed simultaneously.");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return new StringRepresentation(answer.toString());
+
+			}
+
+			// tring tool = "minimac/new-simple";// props.get("tool");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+			String id = "job-" + sdf.format(new Date());// props.get("job-name");
+
+			if (publicMode) {
+				id = HashUtil.getMD5(id + "-lukas");
+			}
+
+			String hdfsWorkspace = HdfsUtil.path(getSettings()
+					.getHdfsWorkspace(), user.getUsername());
+			String localWorkspace = FileUtil.path(getSettings()
+					.getLocalWorkspace(), user.getUsername());
+
+			Map<String, String> props = new HashMap<String, String>();
 
 			try {
+				FileItemIterator iterator = parseRequest(entity);
 
-				Map<String, String> params = new HashMap<>();
+				// uploaded files
+				while (iterator.hasNext()) {
 
-				for (WdlParameter input : app.getMapred().getInputs()) {
-					if (props.containsKey(input.getId())) {
-						if (input.getType().equals("checkbox")) {
-							params.put(input.getId(),
-									input.getValues().get("true"));
-						} else {
-							params.put(input.getId(), props.get(input.getId()));
+					FileItemStream item = iterator.next();
+
+					String name = item.getName();
+
+					if (name != null) {
+
+						// file parameter
+						// write local file
+						String tmpFile = getSettings().getTempFilename(
+								item.getName());
+						File file = new File(tmpFile);
+						try {
+							FileUtils.copyInputStreamToFile(item.openStream(),
+									file);
+						} catch (Exception e) {
+							e.printStackTrace();
 						}
+
+						// import into hdfs
+						if (file.exists()) {
+							String entryName = item.getName();
+
+							// remove upload indentification!
+							String fieldName = item.getFieldName()
+									.replace("-upload", "")
+									.replace("input-", "");
+
+							boolean hdfs = false;
+							boolean folder = false;
+
+							for (WdlParameter input : app.getMapred()
+									.getInputs()) {
+								if (input.getId().equals(fieldName)) {
+									hdfs = (input.getType().equals(
+											WdlParameter.HDFS_FOLDER) || input
+											.getType().equals(
+													WdlParameter.HDFS_FILE));
+									folder = (input.getType()
+											.equals(WdlParameter.HDFS_FOLDER))
+											|| (input.getType()
+													.equals(WdlParameter.LOCAL_FOLDER));
+								}
+							}
+
+							if (hdfs) {
+
+								String targetPath = HdfsUtil.path(
+										hdfsWorkspace, "input", id, fieldName);
+
+								String target = HdfsUtil.path(targetPath,
+										entryName);
+
+								HdfsUtil.put(tmpFile, target);
+
+								// deletes temporary file
+								FileUtil.deleteFile(tmpFile);
+
+								if (folder) {
+									// folder
+									props.put(fieldName, HdfsUtil.path("input",
+											id, fieldName));
+								} else {
+									// file
+									props.put(fieldName, HdfsUtil.path("input",
+											id, fieldName, entryName));
+								}
+
+							} else {
+
+								String targetPath = FileUtil.path(
+										localWorkspace, "input", id, fieldName);
+
+								FileUtil.createDirectory(FileUtil.path(
+										localWorkspace, "input"));
+
+								FileUtil.createDirectory(FileUtil.path(
+										localWorkspace, "input", id));
+
+								FileUtil.createDirectory(FileUtil.path(
+										localWorkspace, "input", id, fieldName));
+
+								String target = FileUtil.path(targetPath,
+										entryName);
+
+								FileUtil.copy(tmpFile, target);
+
+								// deletes temporary file
+								FileUtil.deleteFile(tmpFile);
+
+								if (folder) {
+									// folder
+									props.put(
+											fieldName,
+											new File(FileUtil.path(
+													localWorkspace, "input",
+													id, fieldName))
+													.getAbsolutePath());
+								} else {
+									// file
+									props.put(
+											fieldName,
+											new File(FileUtil.path(
+													localWorkspace, "input",
+													id, fieldName, entryName))
+													.getAbsolutePath());
+								}
+
+							}
+
+						}
+
 					} else {
-						if (input.getType().equals("checkbox")) {
-							params.put(input.getId(),
-									input.getValues().get("false"));
-						}
-					}
-				}
 
-				CloudgeneJob job = new CloudgeneJob(user, id, app.getMapred(),
-						params);
-				job.setId(id);
-				job.setName(id);
-				job.setLocalWorkspace(localWorkspace);
-				job.setHdfsWorkspace(hdfsWorkspace);
-				job.setSettings(getSettings());
-				job.setRemoveHdfsWorkspace(getSettings()
-						.isRemoveHdfsWorkspace());
-				job.setApplication(app.getName() + " " + app.getVersion());
-				job.setApplicationId(tool);
-				engine.submit(job);
+						if (item.getFieldName().startsWith("input-")) {
+							String key = item.getFieldName().replace("input-",
+									"");
+
+							String value = Streams.asString(item.openStream());
+							if (!props.containsKey(key)) {
+								// don't override uploaded files
+								props.put(key, value);
+							}
+
+						}
+
+					}
+
+				}
 
 			} catch (Exception e) {
 				e.printStackTrace();
-				return new JSONAnswer(e.getMessage(), false);
+			}
+
+			if (app.getMapred() != null) {
+
+				try {
+
+					Map<String, String> params = new HashMap<>();
+
+					for (WdlParameter input : app.getMapred().getInputs()) {
+						if (props.containsKey(input.getId())) {
+							if (input.getType().equals("checkbox")) {
+								params.put(input.getId(), input.getValues()
+										.get("true"));
+							} else {
+								params.put(input.getId(),
+										props.get(input.getId()));
+							}
+						} else {
+							if (input.getType().equals("checkbox")) {
+								params.put(input.getId(), input.getValues()
+										.get("false"));
+							}
+						}
+					}
+
+					CloudgeneJob job = new CloudgeneJob(user, id,
+							app.getMapred(), params);
+					job.setId(id);
+					job.setName(id);
+					job.setLocalWorkspace(localWorkspace);
+					job.setHdfsWorkspace(hdfsWorkspace);
+					job.setSettings(getSettings());
+					job.setRemoveHdfsWorkspace(getSettings()
+							.isRemoveHdfsWorkspace());
+					job.setApplication(app.getName() + " " + app.getVersion());
+					job.setApplicationId(tool);
+					engine.submit(job);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					return new JSONAnswer(e.getMessage(), false);
+
+				}
 
 			}
 
-		}
-
-		JSONObject answer = new JSONObject();
-		try {
-			answer.put("id", id);	
-			answer.put("success", true);
-			answer.put("message",
-					"Your job was successfully added to the job queue.");
+			JSONObject answer = new JSONObject();
+			try {
+				answer.put("id", id);
+				answer.put("success", true);
+				answer.put("message",
+						"Your job was successfully added to the job queue.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return new StringRepresentation(answer.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		return new StringRepresentation(answer.toString());
-}catch(Exception e){
-	e.printStackTrace();
-	return new StringRepresentation("");
-}
-
+			return new StringRepresentation("");
+		}
 
 	}
 
