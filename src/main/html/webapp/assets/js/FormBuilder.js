435b96327776784d228a6be01368fed87281de4a
lukfor <lukas.forer@i-med.ac.at>
2016-01-26 15:04:01 +0100

    fixed error with local-file in form-builder.

--- src/main/html/webapp/assets/js/FormBuilder.js
+++ src/main/html/webapp/assets/js/FormBuilder.js
@@ -28,7 +28,7 @@ function buildForm(application, element, params, submitButtonText) {
 				buildList(divControls, param);
 			} else if (param.type == "number") {
 				buildNumber(divControls, param);
-			} else if (param.type == "hdfs-file" || param.type == "local-folder") {
+			} else if (param.type == "hdfs-file" || param.type == "local-file") {
 				buildHdfsFile(divControls, param);
 			} else if (param.type == "hdfs-folder" || param.type == "local-folder") {
 				buildHdfsFolder(divControls, param);
