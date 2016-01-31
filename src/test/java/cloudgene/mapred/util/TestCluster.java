483f9dc9f17c2bb887a348916b767a5fe3686939
lukfor <lukas.forer@i-med.ac.at>
2016-01-31 16:10:41 +0100

    Added a mapreduce testcase to validate minimrcluster.

--- src/test/java/cloudgene/mapred/util/TestCluster.java
+++ src/test/java/cloudgene/mapred/util/TestCluster.java
@@ -9,6 +9,10 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hdfs.HdfsConfiguration;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.mapred.ClusterStatus;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MiniMRCluster;
 
 public class TestCluster {
 
@@ -42,15 +46,24 @@ public class TestCluster {
 				testDataCluster1.delete();
 			}
 			conf = new HdfsConfiguration();
-			conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR,
-					testDataCluster1.getAbsolutePath());
+			conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, testDataCluster1.getAbsolutePath());
 			cluster = new MiniDFSCluster.Builder(conf).build();
 			fs = cluster.getFileSystem();
 
 			// set mincluster as default config
 			HdfsUtil.setDefaultConfiguration(conf);
-
-		}
+			System.setProperty("hadoop.log.dir","test-log-dir");
+			MiniMRCluster			mrCluster = new MiniMRCluster(1, fs.getUri().toString(), 1, null, null,
+					new JobConf(conf));
+			JobConf mrClusterConf = mrCluster.createJobConf();
+			HdfsUtil.setDefaultConfiguration(new Configuration(mrClusterConf));
+			
+			System.out.println("------");
+			
+			JobClient client = new JobClient(mrClusterConf);
+			ClusterStatus status = client.getClusterStatus(true);
+			System.out.println(status.getActiveTrackerNames());
+			}
 	}
 
 }
