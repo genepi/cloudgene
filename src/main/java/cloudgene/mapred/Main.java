69d68902477cac7071bed94e7bafa035670fcee0
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2016-06-11 17:08:40 +0200

    update to 0.1.13

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -41,7 +41,7 @@ public class Main implements Daemon {
 
 	// private static final Log log = LogFactory.getLog(Main.class);
 
-	public static final String VERSION = "1.12.0";
+	public static final String VERSION = "1.13.0";
 
 	private Database database;
 
