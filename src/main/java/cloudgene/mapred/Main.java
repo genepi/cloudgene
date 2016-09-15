fd6887d628dfc2b824554d87a2703b6898ea850f
lukfor <lukas.forer@i-med.ac.at>
2016-09-15 09:56:46 +0200

    Updated to version 1.18

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -32,7 +32,7 @@ import cloudgene.mapred.util.Settings;
 
 public class Main implements Daemon {
 
-	public static final String VERSION = "1.17.0";
+	public static final String VERSION = "1.18.0";
 
 	private Database database;
 
