508670ed8542b87f6a4bdf1087e5d2e83eec1698
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-02-21 16:42:00 +0100

    update restlet and ciphers

--- src/main/java/cloudgene/mapred/WebServer.java
+++ src/main/java/cloudgene/mapred/WebServer.java
@@ -74,11 +74,8 @@ public class WebServer extends Component {
 				             + " TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
 				             + " TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
 				             + " TLS_RSA_WITH_AES_256_CBC_SHA"
-				             + " TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA"
 				             + " TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
-				             + " TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
-				             + " TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA"
-				             + " TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA");
+				             + " TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA");
 			}
 
 		}
