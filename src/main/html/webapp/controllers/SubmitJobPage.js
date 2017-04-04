745b6789bcbf649874499922da71210217e00dfc
lukfor <lukas.forer@i-med.ac.at>
2017-04-04 12:26:20 +0200

    Fixed bug with empty csrf-token.

--- src/main/html/webapp/controllers/SubmitJobPage.js
+++ src/main/html/webapp/controllers/SubmitJobPage.js
@@ -23,133 +23,135 @@ SubmitJobPage = can.Control({
 	},
 
 	'submit': function(tr) {
+		try {
+			tool = this.element.find('tool');
 
-		tool = this.element.find('tool');
+			console.log("submit job...");
 
-		console.log("submit job...");
+			// ---- listboxes
 
-		// ---- listboxes
+			// reset
 
-		// reset
+			this.element.find('select').filter(function() {
+				return true;
+			}).closest('.control-group').removeClass("error");
 
-		this.element.find('select').filter(function() {
-			return true;
-		}).closest('.control-group').removeClass("error");
+			this.element.find('select').filter(function() {
+				return true;
+			}).closest('.control-group').find('.help-block').html("");
 
-		this.element.find('select').filter(function() {
-			return true;
-		}).closest('.control-group').find('.help-block').html("");
+			// validation
 
-		// validation
+			var faults2 = this.element.find('select').filter(
+				function() {
+					return $(this).data('required') && $(this).val() === "---empty---" && $(this).attr('disabled') != 'disabled';
+				}).closest('.control-group').addClass('error');
 
-		var faults2 = this.element.find('select').filter(
-			function() {
-				return $(this).data('required') && $(this).val() === "---empty---" && $(this).attr('disabled') != 'disabled';
-			}).closest('.control-group').addClass('error');
+			this.element.find('select').filter(
+				function() {
+					return $(this).data('required') && $(this).val() === "---empty---" && $(this).attr('disabled') != 'disabled';
+				}).closest('.control-group').find('.help-block').html(
+				"This parameter is required.");
 
-		this.element.find('select').filter(
-			function() {
-				return $(this).data('required') && $(this).val() === "---empty---" && $(this).attr('disabled') != 'disabled';
-			}).closest('.control-group').find('.help-block').html(
-			"This parameter is required.");
+			// --- input fields
 
-		// --- input fields
+			// reset
+			this.element.find('input').filter(function() {
+				return true;
+			}).closest('.control-group').removeClass("error");
 
-		// reset
-		this.element.find('input').filter(function() {
-			return true;
-		}).closest('.control-group').removeClass("error");
+			this.element.find('input').filter(function() {
+				return true;
+			}).closest('.control-group').find('.help-block').html("");
 
-		this.element.find('input').filter(function() {
-			return true;
-		}).closest('.control-group').find('.help-block').html("");
+			// validation
 
-		// validation
+			var faults = this.element.find('input').filter(
+				function() {
+					return $(this).data('required') && $(this).val() === "" && $(this).attr('disabled') != 'disabled';
+				}).closest('.control-group').addClass('error');
 
-		var faults = this.element.find('input').filter(
-			function() {
-				return $(this).data('required') && $(this).val() === "" && $(this).attr('disabled') != 'disabled';
-			}).closest('.control-group').addClass('error');
-
-		this.element.find('input').filter(
-			function() {
-				return $(this).data('required') && $(this).val() === "" && $(this).attr('disabled') != 'disabled';
-			}).closest('.control-group').find('.help-block').html(
-			"This parameter is required.");
+			this.element.find('input').filter(
+				function() {
+					return $(this).data('required') && $(this).val() === "" && $(this).attr('disabled') != 'disabled';
+				}).closest('.control-group').find('.help-block').html(
+				"This parameter is required.");
 
 
-		// agb checkboxes
+			// agb checkboxes
 
-		// reset
+			// reset
 
-		var faults3 = this.element.find('input').filter(
-			function() {
-				return $(this).attr('type') === 'checkbox' && $(this).attr('class') === 'agb' && $(this).attr('checked') != 'checked';
-			}).closest('.control-group').addClass('error');
+			var faults3 = this.element.find('input').filter(
+				function() {
+					return $(this).attr('type') === 'checkbox' && $(this).attr('class') === 'agb' && $(this).attr('checked') != 'checked';
+				}).closest('.control-group').addClass('error');
 
-		this.element.find('input').filter(
-			function() {
-				return $(this).attr('type') === 'checkbox' && $(this).attr('class') === 'agb' && $(this).attr('checked') != 'checked';
-			}).closest('.control-group').find('.help-block').html("Please agree to the terms and conditions.");
+			this.element.find('input').filter(
+				function() {
+					return $(this).attr('type') === 'checkbox' && $(this).attr('class') === 'agb' && $(this).attr('checked') != 'checked';
+				}).closest('.control-group').find('.help-block').html("Please agree to the terms and conditions.");
 
-		if (faults.length > 0 || faults2.length > 0 || faults3.length > 0)
-			return false; // if any required are empty, cancel submit
+			if (faults.length > 0 || faults2.length > 0 || faults3.length > 0)
+				return false; // if any required are empty, cancel submit
 
-		$("#waiting-dialog").modal();
+			$("#waiting-dialog").modal();
+			var csrfToken;
+			if (localStorage.getItem("cloudgene")) {
+				try {
 
-		if (localStorage.getItem("cloudgene")) {
-			try {
+					// get data
+					var data = JSON.parse(localStorage.getItem("cloudgene"));
+					csrfToken = data.csrf;
 
-				// get data
-				var data = JSON.parse(localStorage.getItem("cloudgene"));
-				csrfToken =  data.csrf;
-
-			} catch (e) {
-
-			}
-		}
-
-		this.element.find("#parameters").ajaxSubmit({
-			dataType: 'json',
-
-			headers: {
-				"X-CSRF-Token": csrfToken
-			},
-
-			success: function(answer) {
-				if (answer.success) {
-					$("#waiting-dialog").modal('hide');
-					can.route('jobs/:job');
-					can.route.attr({
-						route: 'jobs/:job',
-						job: answer.id,
-						page: 'jobs'
-					});
-				} else {
-					$("#waiting-dialog").modal('hide');
-					new ErrorPage("#content", {
-						status: "",
-						message: answer.message
-					});
+				} catch (e) {
 
 				}
-			},
-
-			error: function(message) {
-				$("#waiting-dialog").modal('hide');
-				new ErrorPage("#content", {
-					status: message.statusText,
-					message: message.responseText
-				});
-
-			},
-
-			uploadProgress: function(event, position, total, percentComplete) {
-				$("#waiting-progress").css("width", percentComplete + "%");
-				console.log(percentComplete + "%");
 			}
-		});
 
+			this.element.find("#parameters").ajaxSubmit({
+				dataType: 'json',
+
+				headers: {
+					"X-CSRF-Token": csrfToken
+				},
+
+				success: function(answer) {
+					if (answer.success) {
+						$("#waiting-dialog").modal('hide');
+						can.route('jobs/:job');
+						can.route.attr({
+							route: 'jobs/:job',
+							job: answer.id,
+							page: 'jobs'
+						});
+					} else {
+						$("#waiting-dialog").modal('hide');
+						new ErrorPage("#content", {
+							status: "",
+							message: answer.message
+						});
+
+					}
+				},
+
+				error: function(message) {
+					$("#waiting-dialog").modal('hide');
+					new ErrorPage("#content", {
+						status: message.statusText,
+						message: message.responseText
+					});
+
+				},
+
+				uploadProgress: function(event, position, total, percentComplete) {
+					$("#waiting-progress").css("width", percentComplete + "%");
+					console.log(percentComplete + "%");
+				}
+			});
+		} catch (e) {
+			console.log(e);
+		}
 		return false;
 	}
 
