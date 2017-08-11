9a33cff1ccf0dfaa42a13f1d81539c8a1f4a35db
lukfor <lukas.forer@i-med.ac.at>
2017-08-11 15:28:33 +0200

    Added missing WDL classes.

--- src/main/java/cloudgene/mapred/wdl/WdlApp.java
+++ src/main/java/cloudgene/mapred/wdl/WdlApp.java
@@ -1,16 +1,13 @@
 package cloudgene.mapred.wdl;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-
+import cloudgene.mapred.util.ApplicationInstaller;
+import cloudgene.mapred.util.Settings;
 import genepi.hadoop.HdfsUtil;
-import genepi.io.FileUtil;
 
 public class WdlApp extends WdlHeader implements Comparable<WdlApp> {
 
@@ -54,85 +51,17 @@ public class WdlApp extends WdlHeader implements Comparable<WdlApp> {
 		return deinstallation;
 	}
 
-	public void install() throws IOException {
-		// todo: set application folders etc....
+	public void install(HashMap<String, String> environment) throws IOException {
 		if (installation != null) {
-			Map<String, String> environment = new HashMap<String, String>();
-			for (Map<String, Object> commandObject : installation) {
-				runCommand(commandObject, environment);
-			}
+			ApplicationInstaller.install(installation, environment);
 		}
 	}
 
-	public void deinstall() throws IOException {
-		// todo: set application folders etc....
+	public void deinstall(HashMap<String, String> environment) throws IOException {
 		if (deinstallation != null) {
-			Map<String, String> environment = new HashMap<String, String>();
-			for (Map<String, Object> commandObject : deinstallation) {
-				runCommand(commandObject, environment);
-			}
+			ApplicationInstaller.install(deinstallation, environment);
+			HdfsUtil.delete(environment.get("hdfs_app_folder"));
 		}
-	}
-
-	public void runCommand(Map<String, Object> commandObject, Map<String, String> environment) throws IOException {
-		if (commandObject.keySet().size() == 1) {
-			for (String command : commandObject.keySet()) {
-				Object parametersObject = commandObject.get(command);
-				Map<String, Object> parameters = (Map) parametersObject;
-				runCommand(command, parameters, environment);
-			}
-		} else {
-			throw new IOException("Unknown command structure.");
-		}
-	}
-
-	public void runCommand(String command, Map<String, Object> parameters, Map<String, String> environment)
-			throws IOException {
-		switch (command) {
-		case "hdfs_import":
-			String source = (String) parameters.get("source");
-			String target = (String) parameters.get("target");
-			runHdfsImport(source, target);
-			break;
-		case "yaml_add":
-			System.out.println("update yaml file " + parameters.get("file"));
-
-			break;
-		case "yaml_remove":
-			System.out.println("update yaml file " + parameters.get("file"));
-
-			break;
-		default:
-			throw new IOException("Unknown command '" + command + "'");
-		}
-	}
-
-	public void runHdfsImport(String source, String target) throws IOException {
-		String tempFilename = FileUtil.path("temp", "hdfs_import.tempfile");
-		if (source.startsWith("http://") || source.startsWith("https://")) {
-			// download
-			FileUtils.copyURLToFile(new URL(source), new File(tempFilename));
-
-			if (source.endsWith(".zip")) {
-				HdfsUtil.createDirectory(target);
-				HdfsUtil.putZip(tempFilename, target);
-			} else if (source.endsWith(".gz")) {
-				HdfsUtil.createDirectory(target);
-				HdfsUtil.putTarGz(tempFilename, target);
-			} else {
-				HdfsUtil.put(tempFilename, target);
-			}
-
-		} else {
-			if (source.endsWith(".zip")) {
-				HdfsUtil.createDirectory(target);
-				HdfsUtil.putZip(source, target);
-			} else {
-				HdfsUtil.put(source, target);
-			}
-		}
-
-		FileUtil.deleteFile(tempFilename);
 	}
 
 	@Override
--- src/main/java/cloudgene/mapred/wdl/WdlReader.java
+++ src/main/java/cloudgene/mapred/wdl/WdlReader.java
@@ -4,29 +4,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
 
+import cloudgene.mapred.util.ApplicationInstaller;
+import genepi.io.FileUtil;
+
 public class WdlReader {
 
-
-	public static WdlApp loadAppFromString(String filename, String content)
-			throws IOException {
+	public static WdlApp loadAppFromString(String filename, String content) throws IOException {
 
 		YamlReader reader = new YamlReader(new StringReader(content));
 
-		reader.getConfig().setPropertyDefaultType(WdlApp.class, "mapred",
-				WdlMapReduce.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "steps",
-				WdlStep.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "setups",
-				WdlStep.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "inputs",
-				WdlParameterInput.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class,
-				"outputs", WdlParameterOutput.class);
+		reader.getConfig().setPropertyDefaultType(WdlApp.class, "mapred", WdlMapReduce.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "steps", WdlStep.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "setups", WdlStep.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "inputs", WdlParameterInput.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "outputs", WdlParameterOutput.class);
 
 		WdlApp app = reader.read(WdlApp.class);
+		reader.close();
 
 		updateApp(filename, app);
 
@@ -38,18 +38,14 @@ public class WdlReader {
 
 		YamlReader reader = new YamlReader(new FileReader(filename));
 
-		reader.getConfig().setPropertyDefaultType(WdlApp.class, "mapred",
-				WdlMapReduce.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "steps",
-				WdlStep.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "setups",
-				WdlStep.class);		
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "inputs",
-				WdlParameterInput.class);
-		reader.getConfig().setPropertyElementType(WdlMapReduce.class,
-				"outputs", WdlParameterOutput.class);
+		reader.getConfig().setPropertyDefaultType(WdlApp.class, "mapred", WdlMapReduce.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "steps", WdlStep.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "setups", WdlStep.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "inputs", WdlParameterInput.class);
+		reader.getConfig().setPropertyElementType(WdlMapReduce.class, "outputs", WdlParameterOutput.class);
 
 		WdlApp app = reader.read(WdlApp.class);
+		reader.close();
 
 		updateApp(filename, app);
 
@@ -66,8 +62,7 @@ public class WdlReader {
 			String mapper = config.getMapper();
 			String reducer = config.getReducer();
 
-			String path = new File(new File(filename).getAbsolutePath())
-					.getParentFile().getAbsolutePath();
+			String path = new File(new File(filename).getAbsolutePath()).getParentFile().getAbsolutePath();
 			config.setPath(path);
 			config.setManifestFile(filename);
 
@@ -85,6 +80,47 @@ public class WdlReader {
 				step.setReducer(reducer);
 				step.setParams(config.getParams());
 				config.getSteps().add(step);
+			}
+
+			// load values from files
+			if (config.getInputs() != null) {
+				for (WdlParameter input : config.getInputs()) {
+					if (input.getType().toLowerCase().equals("list")) {
+						Map<String, String> values = input.getValues();
+						if (values != null) {
+							String source = values.get("source");
+							String array = values.get("array");
+							String key = values.get("key");
+							String value = values.get("value");
+							if (source != null && array != null && key != null && value != null) {
+								input.setValues(new HashMap<String, String>());
+								try {
+									String sourceFilename = FileUtil.path(path, source);
+									if (new File(sourceFilename).exists()) {
+										YamlReader reader = new YamlReader(new FileReader(sourceFilename));
+										Map<String, Object> data = reader.read(Map.class);
+										List<Map> list = ApplicationInstaller.findArray(data, array);
+										if (list != null) {
+											Map<String, String> newValues = new HashMap<String, String>();
+											for (Map map : list) {
+												if (map.get(key) != null && map.get(value) != null) {
+													String newKey = map.get(key).toString();
+													String newValue = map.get(value).toString();
+													newValues.put(newKey, newValue);
+												}
+											}
+											input.setValues(newValues);
+										}
+										reader.close();
+									}
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+
+							}
+						}
+					}
+				}
 			}
 
 		}
