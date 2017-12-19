d2d3e7e170100c6c1664cb19fa55d9ccd3c05272
lukfor <lukas.forer@i-med.ac.at>
2017-12-19 12:00:14 +0100

    Added statistics to jobs list and added "running time" column.

--- src/main/java/cloudgene/mapred/api/v2/admin/GetAllJobs.java
+++ src/main/java/cloudgene/mapred/api/v2/admin/GetAllJobs.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Vector;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import net.sf.json.JsonConfig;
 
 import org.restlet.data.Status;
@@ -30,14 +31,12 @@ public class GetAllJobs extends BaseResource {
 
 		if (user == null) {
 			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			return new StringRepresentation(
-					"The request requires user authentication.");
+			return new StringRepresentation("The request requires user authentication.");
 		}
 
 		if (!user.isAdmin()) {
 			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			return new StringRepresentation(
-					"The request requires administration rights.");
+			return new StringRepresentation("The request requires administration rights.");
 		}
 
 		String state = "";
@@ -81,17 +80,57 @@ public class GetAllJobs extends BaseResource {
 		}
 
 		JsonConfig config = new JsonConfig();
-		config.setExcludes(new String[] { "outputParams", "inputParams",
-				"output", "error", "s3Url", "task",
-				"config", "mapReduceJob", "job", "step", "context",
-				"hdfsWorkspace", "localWorkspace", "logOutFiles", "logs",
-				"removeHdfsWorkspace", "settings", "setupComplete",
-				"stdOutFile", "steps", "workingDirectory", "activationCode",
-				"active", "admin", "awsKey", "awsSecretKey", "exportInputToS3",
-				"exportToS3", "password", "apiToken", "s3Bucket", "saveCredentials","map","reduce" });
-		JSONArray jsonArray = JSONArray.fromObject(jobs, config);
+		config.setExcludes(new String[] { "outputParams", "inputParams", "output", "error", "s3Url", "task", "config",
+				"mapReduceJob", "job", "step", "context", "hdfsWorkspace", "localWorkspace", "logOutFiles", "logs",
+				"removeHdfsWorkspace", "settings", "setupComplete", "stdOutFile", "steps", "workingDirectory",
+				"activationCode", "active", "admin", "awsKey", "awsSecretKey", "exportInputToS3", "exportToS3",
+				"password", "apiToken", "s3Bucket", "saveCredentials", "map", "reduce", "lastLogin", "loginAttempts",
+				"lockedUntil" });
 
-		return new StringRepresentation(jsonArray.toString());
+		JSONObject object = new JSONObject();
+
+		int success = 0;
+		int failed = 0;
+		int pending = 0;
+		int waiting = 0;
+		int canceld = 0;
+		int running = 0;
+
+		for (AbstractJob job : jobs) {
+			if (job.getState() == AbstractJob.STATE_EXPORTING || job.getState() == AbstractJob.STATE_RUNNING) {
+				running++;
+			}
+			if (job.getState() == AbstractJob.STATE_SUCCESS
+					|| job.getState() == AbstractJob.STATE_SUCESS_AND_NOTIFICATION_SEND) {
+				success++;
+			}
+			if (job.getState() == AbstractJob.STATE_FAILED
+					|| job.getState() == AbstractJob.STATE_FAILED_AND_NOTIFICATION_SEND) {
+				failed++;
+			}
+			if (job.getState() == AbstractJob.STATE_DEAD) {
+				pending++;
+			}
+			if (job.getState() == AbstractJob.STATE_WAITING) {
+				waiting++;
+			}
+			if (job.getState() == AbstractJob.STATE_CANCELED) {
+				canceld++;
+			}
+		}
+
+		object.put("count", jobs.size());
+		object.put("success", success);
+		object.put("failed", failed);
+		object.put("pending", pending);
+		object.put("waiting", waiting);
+		object.put("running", running);
+		object.put("canceld", canceld);
+
+		JSONArray jsonArray = JSONArray.fromObject(jobs, config);
+		object.put("data", jsonArray);
+
+		return new StringRepresentation(object.toString());
 
 	}
 }
