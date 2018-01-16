a27ef57ff083e9e6b54665315391813a86a330f0
lukfor <lukas.forer@i-med.ac.at>
2018-01-16 14:22:51 +0100

    Added missing yaml files.

---
+++ test-data/print-hdfs-file-dataset.groovy
@@ -0,0 +1,17 @@
+import genepi.hadoop.common.WorkflowContext
+import genepi.hadoop.HdfsUtil
+
+def run(WorkflowContext context) {
+
+	def dataset = context.getData("dataset");
+	def hdfs = dataset.get("metafile");
+	def tempFile = context.getLocalTemp()+"/file.txt";
+
+	//export
+	HdfsUtil.get(hdfs, tempFile);
+
+	def content = new File(tempFile).text;
+	context.ok(content);
+
+	return true;
+}
