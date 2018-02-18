6062c857cbddddb0e66b8d19c6238911e650bc58
lukfor <lukas.forer@i-med.ac.at>
2018-02-18 19:32:59 +0100

    Updated reset password page.

--- src/main/html/webapp/controllers/ResetPasswordPage.js
+++ src/main/html/webapp/controllers/ResetPasswordPage.js
@@ -2,44 +2,41 @@
 
 ResetPasswordPage = can.Control({
 
-	"init" : function(element, options) {
-		element.hide();
-		element.html(can.view('views/reset-password.ejs'));
-		element.fadeIn();
-	},
+    "init": function(element, options) {
+        element.hide();
+        element.html(can.view('views/reset-password.ejs'));
+        element.fadeIn();
+    },
 
-	'submit' : function() {
+    'submit': function() {
 
-		username = this.element.find("[name='username']");
+        username = this.element.find("[name='username']");
 
-		$.ajax({
-			url : "/api/v2/users/reset",
-			type : "POST",
-			data : this.element.find("#reset-form").serialize(),
-			dataType : 'json',
-			success : function(data) {
-				if (data.success == true) {
+        $.ajax({
+            url: "/api/v2/users/reset",
+            type: "POST",
+            data: this.element.find("#reset-form").serialize(),
+            dataType: 'json',
+            success: function(data) {
+                if (data.success == true) {
 
-					// show okey!!!
-					username.closest('.control-group').removeClass('error');
-					$("#reset-page").hide();
-					$("#success-message").show();
-					$("#success-message").html(data.message);
+                    // show okey
+                    $("#reset-page").hide();
+                    $("#success-message").show();
+                    $("#success-message").html(data.message);
 
-				} else {
-					// shows error
-					username.closest('.control-group').addClass('error');
-					username.closest('.control-group').find('.help-block')
-							.html(data.message);
+                } else {
+                    // shows error
+                    username.addClass('is-invalid');
+                    username.closest('.form-group').find('.invalid-feedback').html(data.message);
+                }
+            },
+            error: function(message) {
+                alert('failure: ' + message);
+            }
+        });
 
-				}
-			},
-			error : function(message) {
-				alert('failure: ' + message);
-			}
-		});
-
-		return false;
-	}
+        return false;
+    }
 
 });
