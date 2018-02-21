ddc630bf73f481a5e4f42aa633f062aba9701fa6
lukfor <lukas.forer@i-med.ac.at>
2018-02-21 10:31:53 +0100

    Fixed uploading dialog.

--- src/main/html/webapp/controllers/SubmitJobPage.js
+++ src/main/html/webapp/controllers/SubmitJobPage.js
@@ -1,95 +1,108 @@
 // controller
 SubmitJobPage = can.Control({
 
-    "init": function(element, options) {
-        that = this;
-        Application.findOne({
-            tool: options.tool
-        }, function(application) {
-            element.hide();
-            element.html(can.view('views/run.ejs', {
-                application: application
-            }));
-            //buildForm(options.tool, "#parameters", details.params, details.submitButton);
-            element.fadeIn();
+  "init": function(element, options) {
+    that = this;
+    Application.findOne({
+      tool: options.tool
+    }, function(application) {
+      element.hide();
+      element.html(can.view('views/run.ejs', {
+        application: application
+      }));
+      element.fadeIn();
 
-        }, function(message) {
-            new ErrorPage(that.element, {
-                status: message.statusText,
-                message: message.responseText
-            });
-        });
+    }, function(message) {
+      new ErrorPage(that.element, {
+        status: message.statusText,
+        message: message.responseText
+      });
+    });
 
-    },
+  },
 
-    '#parameters submit': function(form) {
+  '#parameters submit': function(form) {
+
+    // check required parameters.
+    if (form[0].checkValidity() === false) {
+      form[0].classList.add('was-validated');
+      return false;
+    }
+
+    //show upload dialog
+    var uploadDialog = bootbox.dialog({
+      message: can.view('views/run.uploading.ejs'),
+      closeButton: false,
+      className: 'upload-dialog',
+      shown: false
+    });
+
+    //start uploading when dialog is shown
+    uploadDialog.on('shown.bs.modal', function() {
+
+      var csrfToken;
+      if (localStorage.getItem("cloudgene")) {
         try {
 
-						// check required parameters.
-            if (form[0].checkValidity() === false) {
-                form[0].classList.add('was-validated');
-                return false;
-            }
+          // get data
+          var data = JSON.parse(localStorage.getItem("cloudgene"));
+          csrfToken = data.csrf;
 
-            tool = this.element.find('tool');
-
-            $("#waiting-dialog").modal();
-            var csrfToken;
-            if (localStorage.getItem("cloudgene")) {
-                try {
-
-                    // get data
-                    var data = JSON.parse(localStorage.getItem("cloudgene"));
-                    csrfToken = data.csrf;
-
-                } catch (e) {
-
-                }
-            }
-
-            form.ajaxSubmit({
-                dataType: 'json',
-
-                headers: {
-                    "X-CSRF-Token": csrfToken
-                },
-
-                success: function(answer) {
-                    if (answer.success) {
-                        $("#waiting-dialog").modal('hide');
-                        can.route('jobs/:job');
-                        can.route.attr({
-                            route: 'jobs/:job',
-                            job: answer.id,
-                            page: 'jobs'
-                        });
-                    } else {
-                        $("#waiting-dialog").modal('hide');
-                        new ErrorPage("#content", {
-                            status: "",
-                            message: answer.message
-                        });
-
-                    }
-                },
-
-                error: function(message) {
-                    $("#waiting-dialog").modal('hide');
-                    new ErrorPage("#content", {
-                        status: message.statusText,
-                        message: message.responseText
-                    });
-
-                },
-
-                uploadProgress: function(event, position, total, percentComplete) {
-                    $("#waiting-progress").css("width", percentComplete + "%");
-                }
-            });
         } catch (e) {
-            console.log(e);
+
         }
-        return false;
-    }
+      }
+
+      //submit form and upload files
+      form.ajaxSubmit({
+        dataType: 'json',
+
+        headers: {
+          "X-CSRF-Token": csrfToken
+        },
+
+        success: function(answer) {
+
+          uploadDialog.modal('hide');
+
+          if (answer.success) {
+            can.route('jobs/:job');
+            can.route.attr({
+              route: 'jobs/:job',
+              job: answer.id,
+              page: 'jobs'
+            });
+
+          } else {
+            new ErrorPage("#content", {
+              status: "",
+              message: answer.message
+            });
+
+          }
+        },
+
+        error: function(message) {
+          uploadDialog.modal('hide');
+          new ErrorPage("#content", {
+            status: message.statusText,
+            message: message.responseText
+          });
+
+        },
+
+        //upade progress bar
+        uploadProgress: function(event, position, total, percentComplete) {
+          $("#waiting-progress").css("width", percentComplete + "%");
+        }
+
+      });
+
+    });
+
+    //show upload dialog. fires uploading files.
+    uploadDialog.modal('show');
+    return false;
+  }
 
 });
