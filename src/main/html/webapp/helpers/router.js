617c68615abe36240a56b2e33a1e8dde7bbcca8e
lukfor <lukas.forer@i-med.ac.at>
2018-10-24 10:50:06 +0200

    Fix routing issues and add guards

--- src/main/html/webapp/helpers/router.js
+++ src/main/html/webapp/helpers/router.js
@@ -1,46 +1,68 @@
+
 import Control from 'can-control';
 import canRoute from 'can-route';
 import canMap from 'can-map';
-import ajax from 'can-ajax';
+import 'can-ajax';
 import $ from 'jquery';
 
 
 export default Control.extend({
   defaults: {
-    routingMap: new canMap({
-      index: 0
-    })
+    routingMap: new canMap({}),
+    lastBatchNum: undefined,
+    appState: new canMap(),
+    timeoutMin: 60
   }
 }, {
 
   'init': function(element, options) {
     this.setupAuthentication();
+
     this.element = $(element);
     canRoute.data = options.routingMap;
+    options.routesByPath = {};
     for (var i = 0; i < options.routes.length; i++) {
       var route = options.routes[i];
       canRoute(route.path, {
-        index: i
+        path: route.path
       });
+      options.routesByPath[route.path] = route;
     }
 
+    canRoute.bind('change', this.check)
+    canRoute.router = this;
     canRoute.start();
   },
 
-  '{routingMap} change': function() {
-    var index = canRoute.data.attr('index');
-    var route = this.options.routes[index];
-    var data = new canMap();
-    data.attr(canRoute.data);
-    data.attr(route.options);
-    this.activate({
-      control: route.control,
-      data: data,
-      id: '',
-      classes: route.classes
-    });
+  'check': function(ev, attr, how) {
+    var router = canRoute.router;
+    if ((router.lastBatchNum == undefined || router.lastBatchNum != ev.batchNum) && (canRoute.data.attr('path') != undefined && canRoute.data.attr('path') == canRoute.matched())) {
+      router.lastBatchNum = ev.batchNum;
+      var path = canRoute.data.attr('path');
+      console.log('[Router] Found control for ' + path);
+      var route = router.options.routesByPath[path];
+      var control = route.control;
+      console.log('[Router] Parameters: ');
+      console.log(canRoute.data.attr());
+      var data = new canMap();
+      data.attr(canRoute.data);
+      data.attr(route.options);
+      data.attr('appState', router.options.appState);
+      if (route.guard) {
+        var allowed = route.guard(router.options.appState);
+        if (allowed == false) {
+          control = router.options.forbidden.control;
+          data.attr(router.options.forbidden.options);
+        }
+      }
+      router.activate({
+        control: control,
+        data: data,
+        id: '',
+        classes: route.classes
+      });
+    }
   },
-
 
   activate: function(options) {
 
