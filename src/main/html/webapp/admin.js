ef92482e139577af1ddbce7b419b3ae28b6871c7
lukfor <lukas.forer@i-med.ac.at>
2018-10-29 11:53:35 +0100

    Fix wrong css in admin panel

--- src/main/html/webapp/admin.js
+++ src/main/html/webapp/admin.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'assets/css/cloudgene.css';
+import 'components/core/layout/layout.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'can-map-define';
 
