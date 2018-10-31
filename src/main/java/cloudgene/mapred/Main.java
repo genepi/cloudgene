2e166922168fdd2451509edc7968a0702569e5f0
lukfor <lukas.forer@i-med.ac.at>
2018-10-31 13:15:29 +0100

    Prepare release 1.30.6

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -37,7 +37,7 @@ import cloudgene.mapred.util.Settings;
 
 public class Main implements Daemon {
 
-	public static final String VERSION = "1.30.5";
+	public static final String VERSION = "1.30.6";
 
 	private Database database;
 
