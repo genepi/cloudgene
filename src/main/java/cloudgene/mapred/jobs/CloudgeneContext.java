7c124fed584a59077bbd75245b3294e8558a9fbd
lukas <lukas@lukas-workstation>
2019-06-25 14:49:13 +0200

    Send mails only when mail server is set

--- src/main/java/cloudgene/mapred/jobs/CloudgeneContext.java
+++ src/main/java/cloudgene/mapred/jobs/CloudgeneContext.java
@@ -55,7 +55,7 @@ public class CloudgeneContext extends WorkflowContext {
 	private Map<String, String> config;
 
 	private int chunks = 0;
-	
+
 	public CloudgeneContext(CloudgeneJob job) {
 
 		this.workingDirectory = job.getWorkingDirectory();
@@ -283,9 +283,13 @@ public class CloudgeneContext extends WorkflowContext {
 	public boolean sendMail(String subject, String body) throws Exception {
 		Settings settings = getSettings();
 
-		MailUtil.send(settings.getMail().get("smtp"), settings.getMail().get("port"), settings.getMail().get("user"),
-				settings.getMail().get("password"), settings.getMail().get("name"), user.getMail(),
-				"[" + settings.getName() + "] " + subject, body);
+		if (settings.getMail() != null) {
+
+			MailUtil.send(settings.getMail().get("smtp"), settings.getMail().get("port"),
+					settings.getMail().get("user"), settings.getMail().get("password"), settings.getMail().get("name"),
+					user.getMail(), "[" + settings.getName() + "] " + subject, body);
+
+		}
 
 		return true;
 
@@ -294,10 +298,13 @@ public class CloudgeneContext extends WorkflowContext {
 	public boolean sendMail(String to, String subject, String body) throws Exception {
 		Settings settings = getSettings();
 
+		if (settings.getMail() != null) {
+		
 		MailUtil.send(settings.getMail().get("smtp"), settings.getMail().get("port"), settings.getMail().get("user"),
 				settings.getMail().get("password"), settings.getMail().get("name"), to,
 				"[" + settings.getName() + "] " + subject, body);
 
+		}
 		return true;
 
 	}
@@ -457,7 +464,7 @@ public class CloudgeneContext extends WorkflowContext {
 			return null;
 		}
 	}
-	
+
 	public void addFile(String filename) {
 		chunks++;
 		String chunkFolder = FileUtil.path(getLocalOutput(), "chunks");
@@ -467,6 +474,5 @@ public class CloudgeneContext extends WorkflowContext {
 		FileUtil.copy(filename, FileUtil.path(chunkFolder, chunkFilename));
 		message(chunkFilename, 27);
 	}
-
 
 }
