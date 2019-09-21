ce365c56d28a9105804de51155afb91ac69f0446
lukfor <lukas.forer@i-med.ac.at>
2019-09-21 14:40:36 +0200

    Add auto mapping to nextflow pipelines and add pattern property

--- src/main/java/cloudgene/mapred/api/v2/jobs/SubmitJob.java
+++ src/main/java/cloudgene/mapred/api/v2/jobs/SubmitJob.java
@@ -40,10 +40,8 @@ import genepi.io.FileUtil;
 
 public class SubmitJob extends BaseResource {
 
-
 	private static final Log log = LogFactory.getLog(SubmitJob.class);
 
-	
 	@Post
 	public Representation post(Representation entity) {
 
@@ -54,7 +52,6 @@ public class SubmitJob extends BaseResource {
 		} catch (UnsupportedEncodingException e2) {
 			return error404("Application '" + appId + "' is not in valid format.");
 		}
-
 
 		ApplicationRepository repository = getApplicationRepository();
 		Application application = repository.getByIdAndUser(appId, user);
@@ -99,13 +96,13 @@ public class SubmitJob extends BaseResource {
 		}
 
 		String hdfsWorkspace = "";
-		
+
 		try {
 			hdfsWorkspace = HdfsUtil.path(getSettings().getHdfsWorkspace(), id);
-		}catch (NoClassDefFoundError e) {
+		} catch (NoClassDefFoundError e) {
 			log.warn("Hadoop not found in classpath. Ignore HDFS Workspace.", e);
 		}
-		
+
 		String localWorkspace = FileUtil.path(getSettings().getLocalWorkspace(), id);
 		FileUtil.createDirectory(localWorkspace);
 
@@ -185,17 +182,17 @@ public class SubmitJob extends BaseResource {
 						// remove upload indentification!
 						String fieldName = item.getFieldName().replace("-upload", "").replace("input-", "");
 
-						boolean hdfs = false;
-						boolean folder = false;
+						// boolean hdfs = false;
+						// boolean folder = false;
 
+						WdlParameterInput inputParam = null;
 						for (WdlParameterInput input : app.getWorkflow().getInputs()) {
 							if (input.getId().equals(fieldName)) {
-								hdfs = input.isHdfs();
-								folder = input.isFolder();
+								inputParam = input;
 							}
 						}
 
-						if (hdfs) {
+						if (inputParam.isHdfs()) {
 
 							String targetPath = HdfsUtil.path(hdfsWorkspace, fieldName);
 
@@ -203,7 +200,7 @@ public class SubmitJob extends BaseResource {
 
 							HdfsUtil.put(tmpFile, target);
 
-							if (folder) {
+							if (inputParam.isFolder()) {
 								// folder
 								props.put(fieldName, HdfsUtil.makeAbsolute(HdfsUtil.path(hdfsWorkspace, fieldName)));
 							} else {
@@ -223,9 +220,14 @@ public class SubmitJob extends BaseResource {
 
 							FileUtil.copy(tmpFile, target);
 
-							if (folder) {
+							if (inputParam.isFolder()) {
 								// folder
-								props.put(fieldName, new File(targetPath).getAbsolutePath());
+								if (inputParam.getPattern() != null && !inputParam.getPattern().isEmpty()) {
+									props.put(fieldName,
+											new File(targetPath).getAbsolutePath() + "/" + inputParam.getPattern());
+								} else {
+									props.put(fieldName, new File(targetPath).getAbsolutePath());
+								}
 							} else {
 								// file
 								props.put(fieldName, new File(target).getAbsolutePath());
@@ -276,28 +278,28 @@ public class SubmitJob extends BaseResource {
 
 		}
 		try {
-		for (WdlParameterInput input : app.getWorkflow().getInputs()) {
-			if (!params.containsKey(input.getId())) {
-				if (props.containsKey(input.getId())) {
-					if (input.getTypeAsEnum() == WdlParameterInputType.CHECKBOX) {
-						params.put(input.getId(), input.getValues().get("true"));
+			for (WdlParameterInput input : app.getWorkflow().getInputs()) {
+				if (!params.containsKey(input.getId())) {
+					if (props.containsKey(input.getId())) {
+						if (input.getTypeAsEnum() == WdlParameterInputType.CHECKBOX) {
+							params.put(input.getId(), input.getValues().get("true"));
+						} else {
+							params.put(input.getId(), props.get(input.getId()));
+						}
 					} else {
-						params.put(input.getId(), props.get(input.getId()));
-					}
-				} else {
-					// ignore invisible input parameters
-					if (input.getTypeAsEnum() == WdlParameterInputType.CHECKBOX && input.isVisible()) {
-						params.put(input.getId(), input.getValues().get("false"));
+						// ignore invisible input parameters
+						if (input.getTypeAsEnum() == WdlParameterInputType.CHECKBOX && input.isVisible()) {
+							params.put(input.getId(), input.getValues().get("false"));
+						}
 					}
 				}
 			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+
 		}
-
-	} catch (Exception e) {
-		e.printStackTrace();
-		throw e;
-
-	}
 		return params;
 	}
 
--- src/main/java/cloudgene/mapred/plugins/nextflow/NextflowStep.java
+++ src/main/java/cloudgene/mapred/plugins/nextflow/NextflowStep.java
@@ -1,5 +1,6 @@
 package cloudgene.mapred.plugins.nextflow;
 
+import java.io.File;
 import java.util.List;
 import java.util.Vector;
 
@@ -8,21 +9,29 @@ import cloudgene.mapred.jobs.CloudgeneStep;
 import cloudgene.mapred.jobs.Message;
 import cloudgene.mapred.wdl.WdlStep;
 import genepi.hadoop.common.WorkflowContext;
+import genepi.io.FileUtil;
 import net.sf.json.JSONObject;
 
 public class NextflowStep extends CloudgeneStep {
 
 	private CloudgeneContext context;
-	
+
 	@Override
 	public boolean run(WdlStep step, CloudgeneContext context) {
 
 		this.context = context;
-		
+
 		String script = step.get("script");
 
 		if (script == null) {
-			context.error("No 'script' parameter found. Please use this parameter to define your nf file.");
+			// no script set. try to find a main.nf. IS the default convention of nf-core.
+			script = "main.nf";
+		}
+
+		String scriptPath = FileUtil.path(context.getWorkingDirectory(), script);
+		if (!new File(scriptPath).exists()) {
+			context.error(
+					"Nextflow script '" + scriptPath + "' not found. Please use 'script' to define your nf file.");
 		}
 
 		NextflowBinary nextflow = NextflowBinary.build(context.getSettings());
@@ -31,6 +40,39 @@ public class NextflowStep extends CloudgeneStep {
 		command.add(nextflow.getBinary());
 		command.add("run");
 		command.add(script);
+
+		// TODO: read from "application config"
+		String profile = "docker";
+
+		// TODO: read from "application config"
+		StringBuffer customConfig = new StringBuffer();
+		customConfig.append("params {\n");
+		customConfig.append("	// Limit resources so that this can run on my laptop\n");
+		customConfig.append("	max_cpus = 2\n");
+		customConfig.append("	max_memory = 6.GB\n");
+		customConfig.append("	max_time = 48.h\n");
+		customConfig.append("	genome = \"Custom\"\n");
+		customConfig.append("}\n");
+
+		String configFilename = FileUtil.path(context.getLocalTemp(), "custom.config");
+		FileUtil.writeStringBufferToFile(configFilename, customConfig);
+
+		// set profile
+		if (!profile.isEmpty()) {
+			command.add("-profile");
+			command.add(profile);
+		}
+
+		// set custom configuration
+		command.add("-c");
+		command.add(configFilename);
+
+		String workDir = FileUtil.path(context.getLocalTemp(), "nextflow");
+		FileUtil.createDirectory(workDir);
+		command.add("-w");
+		command.add(workDir);
+
+		// used to defined hard coded params
 		for (String key : step.keySet()) {
 			if (key.startsWith("params.")) {
 				String param = key.replace("params.", "");
@@ -39,24 +81,36 @@ public class NextflowStep extends CloudgeneStep {
 			}
 		}
 
-		command.add("-with-docker");
+		// add all inputs
+		for (String param : context.getInputs()) {
+			String value = context.getInput(param);
+			command.add("--" + param + "=" + value);
+
+		}
+
+		// add all outputs
+		for (String param : context.getOutputs()) {
+			String value = context.getOutput(param);
+			command.add("--" + param + "=" + value);
+
+		}
+
 		command.add("-with-weblog");
 		command.add("http://localhost:8082/api/v2/collect/" + context.getJobId());
 
 		StringBuilder output = null;
 
-
 		try {
 			context.beginTask("Running Nextflow pipeline...");
 			boolean successful = executeCommand(command, context, output);
 			if (successful) {
-						context.endTask(getNextflowInfo(), Message.OK);
-				
+				context.endTask(getNextflowInfo(), Message.OK);
+
 				return true;
 			} else {
 
-					context.endTask(getNextflowInfo(), Message.ERROR);
-				
+				context.endTask(getNextflowInfo(), Message.ERROR);
+
 				return false;
 			}
 		} catch (Exception e) {
@@ -65,24 +119,25 @@ public class NextflowStep extends CloudgeneStep {
 		}
 
 	}
-	
+
 	private String getNextflowInfo() {
 		String job = context.getJobId();
 		List<JSONObject> events = NextflowInfo.getInstance().getEvents(job);
 		String text = "";
-		for (JSONObject event: events) {
+		for (JSONObject event : events) {
 			JSONObject trace = event.getJSONObject("trace");
-			text+=  "[" + trace.getString("hash") + "] "+ trace.getString("name") + ": " + trace.getString("status") + "<br>";
+			text += "[" + trace.getString("hash") + "] " + trace.getString("name") + ": " + trace.getString("status")
+					+ "<br>";
 		}
 		return text;
 	}
-	
+
 	@Override
 	public void updateProgress() {
 		super.updateProgress();
-			String text = getNextflowInfo();
-			context.updateTask(text, WorkflowContext.RUNNING);
-		
+		String text = getNextflowInfo();
+		context.updateTask(text, WorkflowContext.RUNNING);
+
 	}
 
 	@Override
