69ade43b3e0a360d4a2f19325174b9b7235c15b8
Lukas Forer <lukas.forer@i-med.ac.at>
2019-11-05 18:40:34 +0100

    Prepare release 2.0.3

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -37,7 +37,7 @@ import genepi.io.FileUtil;
 
 public class Main implements Daemon {
 
-	public static final String VERSION = "2.0.2";
+	public static final String VERSION = "2.0.3";
 
 	private Database database;
 
