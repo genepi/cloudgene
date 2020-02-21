f23a1713f7e8fe7abf5b403e4d5303373d1c7afa
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2020-02-21 12:25:45 +0100

    Prepare release 2.1.0

--- src/main/java/cloudgene/mapred/Main.java
+++ src/main/java/cloudgene/mapred/Main.java
@@ -37,7 +37,7 @@ import genepi.io.FileUtil;
 
 public class Main implements Daemon {
 
-	public static final String VERSION = "2.0.5";
+	public static final String VERSION = "2.1.0";
 
 	private Database database;
 
