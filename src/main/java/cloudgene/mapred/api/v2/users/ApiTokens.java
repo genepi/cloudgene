9b55abbfc0aaaf5bc5284cca268c56bda561ad1e
seppinho <sebastian.schoenherr@i-med.ac.at>
2022-03-24 15:16:10 +0100

    Migrate ApiTokens ressource

--- src/main/java/cloudgene/mapred/api/v2/users/ApiTokens.java
+++ src/main/java/cloudgene/mapred/api/v2/users/ApiTokens.java
@@ -1,41 +1,50 @@
 package cloudgene.mapred.api.v2.users;
 
-import org.json.JSONObject;
-import org.restlet.data.Status;
-import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
-import org.restlet.resource.Delete;
-import org.restlet.resource.Get;
-import org.restlet.resource.Post;
-
+import cloudgene.mapred.Application;
 import cloudgene.mapred.core.ApiToken;
 import cloudgene.mapred.core.User;
 import cloudgene.mapred.database.UserDao;
 import cloudgene.mapred.representations.JSONAnswer;
-import cloudgene.mapred.util.BaseResource;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.exceptions.HttpStatusException;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import jakarta.inject.Inject;
 
-public class ApiTokens extends BaseResource {
+import java.security.Principal;
 
-	@Post
-	public Representation createApiKey(Representation entity) {
-		User user = getAuthUser();
+import org.json.JSONObject;
 
-		if (user == null) {
+@Controller
+public class ApiTokens {
 
-			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			return new StringRepresentation(
-					"The request requires user authentication.");
+	@Inject
+	protected Application application;
+	
+	@Post(uri="/api/v2/users/{user}/api-token", consumes = MediaType.ALL)
+	@Secured(SecurityRule.IS_AUTHENTICATED) 
+	public String createApiKey(String user, @Nullable Principal principal) {
 
+		User userObject = application.getUserByPrincipal(principal);
+
+		if (userObject == null) {
+			throw new HttpStatusException(HttpStatus.UNAUTHORIZED, "The request requires user authentication.");
 		}
 
 		// create token
-		String token = ApiToken.create(user,getSettings().getSecretKey());
+		String token = ApiToken.create(userObject, application.getSettings().getSecretKey());
 
 		// update token
-		user.setApiToken(token);
+		userObject.setApiToken(token);
 
-		UserDao userDao = new UserDao(getDatabase());
-		boolean successful = userDao.update(user);
+		UserDao userDao = new UserDao(application.getDatabase());
+		boolean successful = userDao.update(userObject);
 
 		if (successful) {
 
@@ -45,54 +54,50 @@ public class ApiTokens extends BaseResource {
 			answer.put("message", "Creation successfull.");
 			answer.put("token", token);
 			answer.put("type", "plain");
-			return new StringRepresentation(answer.toString());
+			return answer.toString();
 
 		} else {
 
-			return new JSONAnswer("Error during API token generation.", false);
+			return new JSONAnswer("Error during API token generation.", false).toString();
 
 		}
 
 	}
 
-	@Get
-	public Representation getApiKey(Representation entity) {
-		User user = getAuthUser();
+	@Get("/api/v2/users/{user}/api-token")
+	@Secured(SecurityRule.IS_AUTHENTICATED) 
+	public String getApiKey(String user, @Nullable Principal principal) {
 
-		if (user == null) {
+		User userObject = application.getUserByPrincipal(principal);
 
-			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			return new StringRepresentation(
-					"The request requires user authentication.");
-
+		if (userObject == null) {
+			throw new HttpStatusException(HttpStatus.UNAUTHORIZED, "The request requires user authentication.");
 		}
 
 		// return token
 		JSONObject answer = new JSONObject();
 		answer.put("success", true);
-		answer.put("token", user.getApiToken());
+		answer.put("token", userObject.getApiToken());
 		answer.put("type", "plain");
-		return new StringRepresentation(answer.toString());
+		return answer.toString();
 
 	}
 
-	@Delete
-	public Representation revokeApiKey(Representation entity) {
-		User user = getAuthUser();
+	@Delete("/api/v2/users/{user}/api-token")
+	@Secured(SecurityRule.IS_AUTHENTICATED) 
+	public String revokeApiKey(String user, @Nullable Principal principal) {
 
-		if (user == null) {
+		User userObject = application.getUserByPrincipal(principal);
 
-			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			return new StringRepresentation(
-					"The request requires user authentication.");
-
+		if (userObject == null) {
+			throw new HttpStatusException(HttpStatus.UNAUTHORIZED, "The request requires user authentication.");
 		}
 
 		// update token
-		user.setApiToken("");
+		userObject.setApiToken("");
 
-		UserDao userDao = new UserDao(getDatabase());
-		boolean successful = userDao.update(user);
+		UserDao userDao = new UserDao(application.getDatabase());
+		boolean successful = userDao.update(userObject);
 
 		if (successful) {
 
@@ -102,11 +107,11 @@ public class ApiTokens extends BaseResource {
 			answer.put("message", "Creation successfull.");
 			answer.put("token", "");
 			answer.put("type", "plain");
-			return new StringRepresentation(answer.toString());
+			return answer.toString();
 
 		} else {
 
-			return new JSONAnswer("Error during API token generation.", false);
+			return new JSONAnswer("Error during API token generation.", false).toString();
 
 		}
 
