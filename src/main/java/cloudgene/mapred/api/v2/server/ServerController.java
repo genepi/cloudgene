d99624e60ae8f51f2ac5d2c376f2260c46bad711
Lukas Forer <lukas.forer@i-med.ac.at>
2022-03-30 14:02:01 +0200

    Implement CounterResponse object and move counters to ServerController

--- src/main/java/cloudgene/mapred/api/v2/server/ServerController.java
+++ src/main/java/cloudgene/mapred/api/v2/server/ServerController.java
@@ -8,6 +8,8 @@ import cloudgene.mapred.apps.ApplicationRepository;
 import cloudgene.mapred.auth.AuthenticationService;
 import cloudgene.mapred.auth.AuthenticationType;
 import cloudgene.mapred.core.User;
+import cloudgene.mapred.database.UserDao;
+import cloudgene.mapred.responses.CounterResponse;
 import cloudgene.mapred.util.Template;
 import cloudgene.mapred.wdl.WdlApp;
 import io.micronaut.core.annotation.Nullable;
@@ -19,8 +21,8 @@ import io.micronaut.security.rules.SecurityRule;
 import jakarta.inject.Inject;
 import net.sf.json.JSONObject;
 
-@Controller
-public class Server {
+@Controller("/api/v2/server")
+public class ServerController {
 
 	@Inject
 	protected Application application;
@@ -28,9 +30,9 @@ public class Server {
 	@Inject
 	protected AuthenticationService authenticationService;
 	
-	@Get("/api/v2/server")
+	@Get("/")
 	@Secured(SecurityRule.IS_ANONYMOUS)
-	public String getServer(@Nullable Authentication authentication) {
+	public String get(@Nullable Authentication authentication) {
 
 		User user = authenticationService.getUserByAuthentication(authentication, AuthenticationType.ALL_TOKENS);
 
@@ -95,5 +97,22 @@ public class Server {
 		return data.toString();
 
 	}
+	
+	@Get("/counters")
+	@Secured(SecurityRule.IS_ANONYMOUS)
+	public CounterResponse counters() {
+
+		CounterResponse response = CounterResponse.build(application.getWorkflowEngine());
+
+		// TODO: implement a countAll method to avoid creating objects for all users!!
+		// or cache number of users to avoid sql query on each load
+		UserDao dao = new UserDao(application.getDatabase());
+		response.setUsers(dao.findAll().size());
+
+		return response;
+
+	}
+	
+	//TODO: add getStatistics
 
 }
---
+++ src/main/java/cloudgene/mapred/responses/CounterResponse.java
@@ -0,0 +1,59 @@
+package cloudgene.mapred.responses;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cloudgene.mapred.jobs.AbstractJob;
+import cloudgene.mapred.jobs.WorkflowEngine;
+
+public class CounterResponse {
+
+	private Map<String, Long> complete = new HashMap<String, Long>();
+	
+	private Map<String, Long> running = new HashMap<String, Long>();
+	
+	private Map<String, Long> waiting = new HashMap<String, Long>();
+
+	private int users = 0;
+
+	public Map<String, Long> getComplete() {
+		return complete;
+	}
+
+	public void setComplete(Map<String, Long> complete) {
+		this.complete = complete;
+	}
+
+	public Map<String, Long> getRunning() {
+		return running;
+	}
+
+	public void setRunning(Map<String, Long> running) {
+		this.running = running;
+	}
+
+	public Map<String, Long> getWaiting() {
+		return waiting;
+	}
+
+	public void setWaiting(Map<String, Long> waiting) {
+		this.waiting = waiting;
+	}
+
+	public int getUsers() {
+		return users;
+	}
+
+	public void setUsers(int users) {
+		this.users = users;
+	}
+	
+	public static CounterResponse build(WorkflowEngine workflowEngine) {
+		CounterResponse response = new CounterResponse();
+		response.complete = workflowEngine.getCounters(AbstractJob.STATE_CANCELED);
+		response.running = workflowEngine.getCounters(AbstractJob.STATE_RUNNING);
+		response.waiting = workflowEngine.getCounters(AbstractJob.STATE_WAITING);
+		return response;
+	}
+	
+}
