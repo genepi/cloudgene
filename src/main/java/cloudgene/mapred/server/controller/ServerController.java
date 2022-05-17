2b1e4b9e5ba5ad099e95d7236612a40be86589c0
seppinho <sebastian.schoenherr@i-med.ac.at>
2022-05-17 12:05:05 +0200

    Migrate to jackson library for json response

--- src/main/java/cloudgene/mapred/server/controller/ServerController.java
+++ src/main/java/cloudgene/mapred/server/controller/ServerController.java
@@ -49,69 +49,8 @@ public class ServerController {
 		if (authentication != null) {
 			user = authenticationService.getUserByAuthentication(authentication, AuthenticationType.ALL_TOKENS);
 		}
-
-		JSONObject data = new JSONObject();
-		data.put("name", application.getSettings().getName());
-		data.put("background", application.getSettings().getColors().get("background"));
-		data.put("foreground", application.getSettings().getColors().get("foreground"));
-		data.put("footer", application.getTemplate(Template.FOOTER));
-
-		List<String> authClients = new Vector<String>();
-		for (OauthClientConfigurationProperties client : clients) {
-			authClients.add(client.getName());
-		}
-		data.put("oauth", authClients);
-
-		if (user != null) {
-			JSONObject userJson = new JSONObject();
-			userJson.put("username", user.getUsername());
-			userJson.put("mail", user.getMail());
-			userJson.put("admin", user.isAdmin());
-			userJson.put("name", user.getFullName());
-			data.put("user", userJson);
-
-			ApplicationRepository repository = application.getSettings().getApplicationRepository();
-			List<WdlApp> apps = repository.getAllByUser(user, ApplicationRepository.APPS);
-			data.put("apps", apps);
-
-			List<JSONObject> appsJson = new Vector<JSONObject>();
-			List<JSONObject> deprecatedAppsJson = new Vector<JSONObject>();
-			List<JSONObject> experimentalAppsJson = new Vector<JSONObject>();
-
-			for (WdlApp app : apps) {
-				JSONObject appJson = new JSONObject();
-				appJson.put("id", app.getId());
-				appJson.put("name", app.getName());
-				if (app.getRelease() == null) {
-					appsJson.add(appJson);
-				} else if (app.getRelease().equals("deprecated")) {
-					deprecatedAppsJson.add(appJson);
-				} else if (app.getRelease().equals("experimental")) {
-					experimentalAppsJson.add(appJson);
-				} else {
-					appsJson.add(appJson);
-				}
-			}
-
-			data.put("apps", appsJson);
-			data.put("deprecatedApps", deprecatedAppsJson);
-			data.put("experimentalApps", experimentalAppsJson);
-			data.put("loggedIn", true);
-
-		} else {
-			data.put("apps", new Vector<JSONObject>());
-			data.put("loggedIn", false);
-		}
-
-		data.put("navigation", application.getSettings().getNavigation());
-		if (application.getSettings().isMaintenance()) {
-			data.put("maintenace", true);
-			data.put("maintenaceMessage", application.getTemplate(Template.MAINTENANCE_MESSAGE));
-		} else {
-			data.put("maintenace", false);
-		}
-
-		return data.toString();
+		
+		return serverService.getRoot(user);
 
 	}
 
