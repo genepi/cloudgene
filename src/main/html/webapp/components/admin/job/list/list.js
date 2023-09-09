243d9ffe600e9bba5af16c8c0eb39c9c33caaf2e
Lukas Forer <lukas.forer@i-med.ac.at>
2023-09-09 13:08:31 +0200

    Remove preprocessing queue

--- src/main/html/webapp/components/admin/job/list/list.js
+++ src/main/html/webapp/components/admin/job/list/list.js
@@ -11,13 +11,12 @@ import template from './list.stache';
 
 export default Control.extend({
 
-  "init": function(element, options) {
+  "init": function (element, options) {
     $(element).html(template());
     $(element).fadeIn();
 
-    new JobTable("#job-list-running-stq", {state: "running-stq"});
-    new JobTable("#job-list-running-ltq", {state: "running-ltq"});
-    new JobTable("#job-list-current", {state: "current"});
+    new JobTable("#job-list-running-ltq", { state: "running-ltq" });
+    new JobTable("#job-list-current", { state: "current" });
 
   }
 
